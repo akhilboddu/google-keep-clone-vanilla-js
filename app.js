86f491091169e7a40068b6906ee3409396a27e47
akhilboddu <akhil1234mara@gmail.com>
2021-01-20 23:03:50 +0300

    storing notes in the app

--- app.js
+++ app.js
@@ -1,38 +1,63 @@
 class App {
   constructor() {
-    this.$form = document.querySelector('#form');
-    this.$noteTitle = document.querySelector('#note-title');
-    this.$formButtons = document.querySelector('#form-buttons');
-    
+    this.notes = [];  
+      
+    this.$form = document.querySelector("#form");
+    this.$noteTitle = document.querySelector("#note-title");
+    this.$noteText = document.querySelector("#note-text");
+    this.$formButtons = document.querySelector("#form-buttons");
+
     this.addEventListeners();
   }
-  
+
   addEventListeners() {
-    document.body.addEventListener('click', event => {
+    document.body.addEventListener("click", event => {
       this.handleFormClick(event);
     });
-  }
-  
-  handleFormClick(event) {
-    const isFormClicked = this.$form.contains(event.target); 
     
+    this.$form.addEventListener('submit', event => {
+      event.preventDefault(); 
+      const title = this.$noteTitle.value;   
+      const text = this.$noteText.value;   
+      const hasNote = title || text;
+      if (hasNote) {
+        // add note  
+        this.addNote({ title, text });
+      }
+    });
+  }
+
+  handleFormClick(event) {
+    const isFormClicked = this.$form.contains(event.target);
+
     if (isFormClicked) {
       this.openForm();
     } else {
       this.closeForm();
     }
   }
-  
+
   openForm() {
-    this.$form.classList.add('form-open');  
-    this.$noteTitle.style.display = 'block';
-    this.$formButtons.style.display = 'block';
+    this.$form.classList.add("form-open");
+    this.$noteTitle.style.display = "block";
+    this.$formButtons.style.display = "block";
+  }
+
+  closeForm() {
+    this.$form.classList.remove("form-open");
+    this.$noteTitle.style.display = "none";
+    this.$formButtons.style.display = "none";
   }
   
-  closeForm() {
-    this.$form.classList.remove('form-open');  
-    this.$noteTitle.style.display = 'none';
-    this.$formButtons.style.display = 'none';  
+  addNote(note) {
+    const newNote = {
+      title: note.title,
+      text: note.text,
+      color: 'white',
+      id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
+    };
+    this.notes = [...this.notes, newNote];
+    console.log(this.notes);
   }
 }
 
