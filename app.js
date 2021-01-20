c9db9204a27ee2b0ac8c56c2c9b398eed3725552
akhilboddu <akhil1234mara@gmail.com>
2021-01-20 23:19:08 +0300

    displaying notes in the app

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 class App {
   constructor() {
-    this.notes = [];  
-      
+    this.notes = [];
+
+    this.$placeholder = document.querySelector('#placeholder');
     this.$form = document.querySelector("#form");
+    this.$notes = document.querySelector('#notes');
     this.$noteTitle = document.querySelector("#note-title");
     this.$noteText = document.querySelector("#note-text");
     this.$formButtons = document.querySelector("#form-buttons");
@@ -14,14 +16,14 @@ class App {
     document.body.addEventListener("click", event => {
       this.handleFormClick(event);
     });
-    
-    this.$form.addEventListener('submit', event => {
-      event.preventDefault(); 
-      const title = this.$noteTitle.value;   
-      const text = this.$noteText.value;   
+
+    this.$form.addEventListener("submit", event => {
+      event.preventDefault();
+      const title = this.$noteTitle.value;
+      const text = this.$noteText.value;
       const hasNote = title || text;
       if (hasNote) {
-        // add note  
+        // add note
         this.addNote({ title, text });
       }
     });
@@ -47,17 +49,38 @@ class App {
     this.$form.classList.remove("form-open");
     this.$noteTitle.style.display = "none";
     this.$formButtons.style.display = "none";
+    this.$noteTitle.value = '';
+    this.$noteText.value = '';
   }
-  
+
   addNote(note) {
     const newNote = {
       title: note.title,
       text: note.text,
-      color: 'white',
+      color: "white",
       id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
     };
     this.notes = [...this.notes, newNote];
-    console.log(this.notes);
+    this.displayNotes();
+    this.closeForm();
+  }
+  
+  displayNotes() {
+     const hasNotes = this.notes.length > 0;  
+     this.$placeholder.style.display = hasNotes ? 'none' : 'flex';  
+   
+     this.$notes.innerHTML = this.notes.map(note => `
+        <div style="background: ${note.color};" class="note">
+          <div class="${note.title && 'note-title'}">${note.title}</div>
+          <div class="note-text">${note.text}</div>
+          <div class="toolbar-container">
+            <div class="toolbar">
+              <img class="toolbar-color" src="https://icon.now.sh/palette">
+              <img class="toolbar-delete" src="https://icon.now.sh/delete">
+            </div>
+          </div>
+        </div>
+     `).join("");
   }
 }
 
